8333901fae8028cd37b2431cc6c5630da496a800
theryanclausen <theryanclausen@gmail.com>
2019-03-27 21:07:37 -0700

    todos render through todo-item component

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -7,7 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'Angular To-Do';
-  constructor(){
-    console.log('nah')
-  }
+
 }
--- todolist/src/app/component/todos/todos.component.ts
+++ todolist/src/app/component/todos/todos.component.ts
@@ -23,7 +23,7 @@ export class TodosComponent implements OnInit {
       },
       {
         id: 3,
-        title: "Item a",
+        title: "Item x",
         completed: true
       },
       {
